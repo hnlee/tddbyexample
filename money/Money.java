f7597fceac7af0ba6c48e441c033088e35c6c13d
Hana Lee <hanalee07@gmail.com>
2016-07-18 16:04:38 -0500

    Refactor to include currency instance variable and remove redundancies

--- money/Money.java
+++ money/Money.java
@@ -1,34 +1,42 @@
 abstract class Money {
     protected int amount;
+    protected String currency;
+    Money(int amount, String currency) {
+        this.amount = amount;
+        this.currency = currency;
+    }
     public boolean equals(Object object) {
         Money money = (Money) object;
         return amount == money.amount
             && getClass().equals(money.getClass());
     }
     static Money dollar(int amount) {
-        return new Dollar(amount);
+        return new Dollar(amount, "USD");
     }
     static Money franc(int amount) {
-        return new Franc(amount);
+        return new Franc(amount, "CHF");
     }
     abstract Money times(int multiplier);
+    String currency() {
+        return currency;
+    };
 
 }
 
 class Dollar extends Money {
-    Dollar (int amount) {
-        this.amount = amount;
+    Dollar (int amount, String currency) {
+        super(amount, currency);
     }
     Money times(int multiplier) {
-        return new Dollar(amount * multiplier);
+        return Money.dollar(amount * multiplier);
     }
 }
 
 class Franc extends Money {
-    Franc (int amount) {
-        this.amount = amount;
+    Franc (int amount, String currency) {
+        super(amount, currency);
     }
     Money times(int multiplier) {
-        return new Franc(amount * multiplier);
+        return Money.franc(amount * multiplier);
     }
 }
