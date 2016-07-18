93d36f5aa873b2457b80c55d4c1ef6787edf83b8
Hana Lee <hanalee07@gmail.com>
2016-07-18 16:56:24 -0500

    Write test for addition method and placeholder code to compile

--- money/Money.java
+++ money/Money.java
@@ -1,4 +1,4 @@
-class Money {
+class Money implements Expression {
     protected int amount;
     protected String currency;
     Money(int amount, String currency) {
@@ -19,10 +19,22 @@ class Money {
     Money times(int multiplier) {
         return new Money(amount * multiplier, currency);
     }
+    Expression plus(Money addend) {
+        return new Money(amount + addend.amount, currency);
+    }
     String currency() {
         return currency;
     }
     public String toString() {
         return amount + " " + currency;
+    }
+}
+
+interface Expression {
+}
+
+class Bank {
+    Money reduce(Expression source, String to) {
+        return null;
     }
 }
--- money/MoneyTest.java
+++ money/MoneyTest.java
@@ -10,13 +10,6 @@ public class MoneyTest {
     }
 
     @Test
-    public void testFrancMultiplication() {
-        Money five = Money.franc(5);
-        assertEquals(Money.franc(10), five.times(2));
-        assertEquals(Money.franc(15), five.times(3)); 
-    }
-
-    @Test
     public void testEquality() {
         assertTrue(Money.dollar(5).equals(Money.dollar(5)));
         assertFalse(Money.dollar(5).equals(Money.dollar(6)));
@@ -28,4 +21,14 @@ public class MoneyTest {
         assertEquals("USD", Money.dollar(1).currency());
         assertEquals("CHF", Money.franc(1).currency());
     }
+
+    @Test
+    public void testSimpleAddition() {
+        Money five = Money.dollar(5);
+        Expression sum = five.plus(five);
+        Bank bank = new Bank();
+        Money reduced = bank.reduce(sum, "USD");
+        assertEquals(Money.dollar(10), reduced);
+    }
+
 }
