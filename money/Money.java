40dfec3860f6262242c4037ad47e3d4275757ff2
Hana Lee <hanalee07@gmail.com>
2016-07-18 15:23:08 -0500

    Make Money superclass, move equals method to it

--- money/Money.java
+++ money/Money.java
@@ -1,13 +1,25 @@
-class Dollar {
-    private int amount;
+class Money {
+    protected int amount;
+    public boolean equals(Object object) {
+        Money money = (Money) object;
+        return amount == money.amount;
+    }
+}
+
+class Dollar extends Money {
     Dollar (int amount) {
         this.amount = amount;
     }
     Dollar times(int multiplier) {
         return new Dollar(amount * multiplier);
     }
-    public boolean equals(Object object) {
-        Dollar dollar = (Dollar) object;
-        return amount == dollar.amount;
+}
+
+class Franc extends Money {
+    Franc (int amount) {
+        this.amount = amount;
+    }
+    Franc times(int multiplier) {
+        return new Franc(amount * multiplier);
     }
 }
