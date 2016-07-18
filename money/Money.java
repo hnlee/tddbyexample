a5db68647884a9601c41f6572b47cab5ab8e50af
Hana Lee <hanalee07@gmail.com>
2016-07-18 14:44:49 -0500

    Put in equality method and test, rewrite multiplication test

--- money/Money.java
+++ money/Money.java
@@ -1,9 +1,13 @@
 class Dollar {
+    int amount;
     Dollar (int amount) {
         this.amount = amount;
     }
-    int amount;
     Dollar times(int multiplier) {
         return new Dollar(amount * multiplier);
+    }
+    public boolean equals(Object object) {
+        Dollar dollar = (Dollar) object;
+        return amount == dollar.amount;
     }
 }
