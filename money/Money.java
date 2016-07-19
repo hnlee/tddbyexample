2e13d1eacefaa406aeba3f00b3a9c8646ec334b5
Hana Lee <hanalee07@gmail.com>
2016-07-19 10:00:18 -0500

    Move reduce method to Sum so Bank can work with any expression

--- money/Money.java
+++ money/Money.java
@@ -40,12 +40,15 @@ class Sum implements Expression {
         this.augend = augend;
         this.addend = addend;
     }
+    public Money reduce(String to) {
+        int amount = augend.amount + addend.amount;
+        return new Money(amount, to);
+    }
 }
 
 class Bank {
     Money reduce(Expression source, String to) {
         Sum sum = (Sum) source;
-        int amount = sum.augend.amount + sum.addend.amount;
-        return new Money(amount, to);
+        return sum.reduce(to);
     }
 }
